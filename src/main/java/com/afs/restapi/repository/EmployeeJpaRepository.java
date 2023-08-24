ce3bfd86e8820ede1840d181ad6b8ffbacf98e51
Jens Jovellano <jensjovellano@gmail.com>
2023-08-24 16:53:42 +0800

    Feat: Added findByCompanyId function.

--- src/main/java/com/afs/restapi/repository/EmployeeJpaRepository.java
+++ src/main/java/com/afs/restapi/repository/EmployeeJpaRepository.java
@@ -9,4 +9,6 @@ import java.util.List;
 @Repository
 public interface EmployeeJpaRepository extends JpaRepository<Employee,Long> {
     List<Employee> findAllByGender(String gender);
+
+    List<Employee> findByCompanyId(Long id);
 }
